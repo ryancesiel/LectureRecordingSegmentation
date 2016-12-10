d3a8aa8f1b26bcda0c68de94c5a02a80af465022
Ryan Cesiel <ryan.cesiel@gmail.com>
2016-12-10 14:11:24 -0500

    noun phrase comparison

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from nltk.tokenize.texttiling import TextTilingTokenizer
 import re
 import math
 import numpy
+from textblob import TextBlob
 
 def plot(s, ss, d, b, t):
     print (b, t)
@@ -72,7 +73,7 @@ def identify_boundaries(tt, depth_scores, percent = 80, boundary_diff = 5):
     depth_tuples = sorted(zip(depth_scores, range(len(depth_scores))))
 
     depth_tuples.reverse()
-    
+
 
     hp = list(filter(lambda x:x[0]>cutoff, depth_tuples))
     # print (len(depth_tuples), len(hp))
@@ -85,6 +86,73 @@ def identify_boundaries(tt, depth_scores, percent = 80, boundary_diff = 5):
                 boundaries[dt[1]] = 0
     return boundaries
 
+def _np_block_comparison(tokseqs, noun_phrases):
+    "Implements the block comparison method"
+    TT_K = 30
+
+    # FROM _create_token_table
+    np_table = {}
+    current_tok_seq = 0
+    for ts in tokseqs:
+        ts_tb = TextBlob(' '.join([word[0] for word in ts.wrdindex_list]))
+        for np in ts_tb.noun_phrases:
+            if np in np_table:
+                np_table[np]['total_count'] += 1
+
+                if np_table[np]['last_tok_seq'] != current_tok_seq:
+                    np_table[np]['last_tok_seq'] = current_tok_seq
+                    np_table[np]['ts_occurences'].append([current_tok_seq,1])
+                else:
+                    np_table[np]['ts_occurences'][-1][1] += 1
+            else: #new word
+                np_table[np] = {
+                    'ts_occurences': [[current_tok_seq,1]],
+                    'total_count': 1,
+                    'last_tok_seq': current_tok_seq
+                }
+
+        current_tok_seq += 1
+    # END _create_token_table
+
+    def blk_frq(np, block):
+        ts_occs = filter(lambda o: o[0] in block,
+                         np_table[np]['ts_occurences'])
+        freq = sum([tsocc[1] for tsocc in ts_occs])
+        return freq
+
+    gap_scores = []
+    numgaps = len(tokseqs)-1
+
+    for curr_gap in range(numgaps):
+        score_dividend, score_divisor_b1, score_divisor_b2 = 0.0, 0.0, 0.0
+        score = 0.0
+        #adjust window size for boundary conditions
+        if curr_gap < TT_K-1:
+            window_size = curr_gap + 1
+        elif curr_gap > numgaps-TT_K:
+            window_size = numgaps - curr_gap
+        else:
+            window_size = TT_K
+
+        b1 = [ts.index
+              for ts in tokseqs[curr_gap-window_size+1 : curr_gap+1]]
+        b2 = [ts.index
+              for ts in tokseqs[curr_gap+1 : curr_gap+window_size+1]]
+
+        for np in np_table:
+            score_dividend += blk_frq(np, b1)*blk_frq(np, b2)
+            score_divisor_b1 += blk_frq(np, b1)**2
+            score_divisor_b2 += blk_frq(np, b2)**2
+        try:
+            score = score_dividend/math.sqrt(score_divisor_b1*
+                                             score_divisor_b2)
+        except ZeroDivisionError:
+            pass # score += 0.0
+
+        gap_scores.append(score)
+
+    return gap_scores
+
 def tokenize(tt, text, targets, percent = 80, boundary_diff = 5):
 
     lowercase_text = text.lower()
@@ -96,11 +164,11 @@ def tokenize(tt, text, targets, percent = 80, boundary_diff = 5):
     # Remove punctuation
     nopunct_text = ''.join(c for c in lowercase_text
                            if re.match("[a-z\-\' \n\t]", c))
-    # print (nopunct_text)
+
     nopunct_par_breaks = tt._mark_paragraph_breaks(nopunct_text)
 
     tokseqs = tt._divide_to_tokensequences(nopunct_text)
-    
+
     target_boundry = find_target_boundry(tt, nopunct_text, targets)
     _divide_to_tokensequences(tt, nopunct_text)
     # print (nopunct_text)
@@ -115,12 +183,17 @@ def tokenize(tt, text, targets, percent = 80, boundary_diff = 5):
 
     # Lexical score determination
     gap_scores = tt._block_comparison(tokseqs, token_table)
+    np_gap_scores = _np_block_comparison(tokseqs, [])
 
     smooth_scores = tt._smooth_scores(gap_scores)
+    np_smooth_scores = tt._smooth_scores(np_gap_scores)
     # End of Lexical score Determination
 
     # Boundary identification
     depth_scores = tt._depth_scores(smooth_scores)
+    np_depth_scores = tt._depth_scores(np_smooth_scores)
+    for idx, np_ds in enumerate(np_depth_scores):
+        depth_scores[idx] = depth_scores[idx] * 0.80 + np_ds * 0.2
 
     # segment_boundaries = tt._identify_boundaries(depth_scores)
 
@@ -193,7 +266,7 @@ def baseline(target_boundry):
 
 text = ""
 targets = []
-with open("MIT_lec_1.train") as f:
+with open("MIT_lec_4.train") as f:
     content = f.readlines()
     for line in content:
         text+=line
@@ -221,13 +294,10 @@ baseline(t)
 #         precision, recall = evaluate(b,t)
 #         print('percent: ', i, 'diff: ', j, precision, recall)
 
-f = open('test.out','w') 
+f = open('test.out','w')
 index = 1
 for para in new_text:
     index+=1
     output = para + '\n\n\n' + '<<BREAK>>' + '\n\n\n'
     f.write(output)
 f.close()
-
-
-
